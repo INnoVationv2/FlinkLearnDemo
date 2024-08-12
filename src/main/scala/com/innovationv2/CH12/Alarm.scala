becd01b1187ad722f730c85d046a8d6fcfa30283
Liu,Yu <code.liuyu@gmail.com>
2024-08-13 00:53:11 +0800

    Add Alarm

---
+++ src/main/scala/com/innovationv2/CH12/Alarm.scala
@@ -0,0 +1,47 @@
+package com.innovationv2.CH12
+
+import com.innovationv2.utils.{LoginEvent, LoginEventSource}
+import org.apache.flink.cep.PatternSelectFunction
+import org.apache.flink.cep.scala.CEP
+import org.apache.flink.cep.scala.pattern.Pattern
+import org.apache.flink.streaming.api.scala._
+import org.junit.Test
+
+import java.util
+
+class Alarm {
+  @Test
+  def testAlarm(): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val stream = env.addSource(new LoginEventSource)
+      .assignAscendingTimestamps(_.timestamp)
+      .keyBy(_.userId)
+
+    val pattern = Pattern
+      .begin[LoginEvent]("first")
+      .where(_.eventType.equals("fail"))
+      .next("second")
+      .where(_.eventType.equals("fail"))
+      .next("third")
+      .where(_.eventType.equals("fail"))
+
+    val patternStream = CEP.pattern(stream, pattern)
+    patternStream
+      .select(new PatternSelectFunction[LoginEvent, String] {
+        override def select(matches: util.Map[String, util.List[LoginEvent]]): String = {
+          val first = matches.get("first").get(0)
+          val second = matches.get("second").get(0)
+          val third = matches.get("third").get(0)
+          s"Alarm: 连续三次登录失败，登录时间: ${first.timestamp},${second.timestamp},${third.timestamp} "
+        }
+      })
+      .print("Alarm: ")
+
+    env.execute()
+  }
+}
+
+
+
