e821557f6c8dc2cae276c72f029577e84ac474f4
Terry <code.liuyu@gmail.com>
2024-08-12 16:49:42 +0800

    Bug fix

--- src/main/scala/com/innovationv2/CH11/OverAggDemo.scala
+++ src/main/scala/com/innovationv2/CH11/OverAggDemo.scala
@@ -50,4 +50,22 @@ class OverAggDemo {
     tableEnv.toDataStream(tableEnv.sqlQuery(sql)).print("testOverAggByTime: ")
     env.execute()
   }
+
+  // 开一个窗口，其中包含本条和前一条数据，求出url较长的长度
+  @Test
+  def testOverWindow(): Unit = {
+    val sql =
+      """SELECT id, user,
+        | SUM(id) OVER w AS sum_id,
+        | MAX(CHAR_LENGTH(url)) OVER w as max_url
+        |FROM EventTable
+        |WINDOW w AS (
+        | ORDER BY ts
+        | ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+        |)
+        |""".stripMargin
+    println(sql)
+    tableEnv.toDataStream(tableEnv.sqlQuery(sql)).print("testOverAggByTime: ")
+    env.execute()
+  }
 }
